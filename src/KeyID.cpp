fcb81d8160542fab6bb10d124cc43f1c14e5b106
James Brown <soylentblue@hotmail.co.uk>
2022-02-09 08:13:38 +1100

    Ensure TokenScript bridge key is generated

--- src/KeyID.cpp
+++ src/KeyID.cpp
@@ -4,14 +4,14 @@
 
 KeyID::KeyID(Web3* web3)
 {
-    recoveredKey = false;
+  recoveredKey = false;
   //private key is two 64 bytes, can do this by generating random bytes until we have 64 hex digits
   std::string privateKey = "";
   privateKeyBytes = new BYTE[ETHERS_PRIVATEKEY_LENGTH];
 
-  if (!EEPROM.begin(EEPROM_SIZE+1))
+  if (!EEPROM.begin(EEPROM_SIZE + 1))
   {
-    Serial.println("failed to initialise EEPROM"); 
+    Serial.println("failed to initialise EEPROM");
     delay(1000000);
     return;
   }
@@ -23,13 +23,18 @@ KeyID::KeyID(Web3* web3)
     Serial.println("Recovering key from EEPROM");
     for (int i = 0; i < ETHERS_PRIVATEKEY_LENGTH; i++)
     {
-      privateKeyBytes[i] = byte(EEPROM.read(i+1));
+      privateKeyBytes[i] = byte(EEPROM.read(i + 1));
     }
 
     privateKey = Util::ConvertBytesToHex(privateKeyBytes, ETHERS_PRIVATEKEY_LENGTH);
     if (privateKey[1] == 'x') privateKey.substr(2);
 
     initPrivateKey(privateKey, web3);
+  }
+  else
+  {
+    //generate private key
+    generatePrivateKey(web3);
   }
 }
 
@@ -39,13 +44,12 @@ void KeyID::generatePrivateKey(Web3* web3)
 
   std::string privateKey = Util::ConvertBytesToHex(privateKeyBytes, ETHERS_PRIVATEKEY_LENGTH);
 
-  Serial.print("Got Private Key: ");
-  Serial.println(privateKey.c_str());
+  Serial.println("Initialised Private Key");
 
   EEPROM.write(0, 64);
   for (int i = 0; i < ETHERS_PRIVATEKEY_LENGTH; i++)
   {
-    EEPROM.write(i+1, privateKeyBytes[i]);
+    EEPROM.write(i + 1, privateKeyBytes[i]);
   }
 
   EEPROM.commit();
@@ -53,40 +57,40 @@ void KeyID::generatePrivateKey(Web3* web3)
   initPrivateKey(privateKey, web3);
 }
 
-void KeyID::getSignature(uint8_t* signature, BYTE *msgBytes, int length)
+void KeyID::getSignature(uint8_t* signature, BYTE* msgBytes, int length)
 {
-    if (recoveredKey == false) return;
-    //need to convert uint32_t to byte
-    uint8_t hash[ETHERS_KECCAK256_LENGTH];
+  if (recoveredKey == false) return;
+  //need to convert uint32_t to byte
+  uint8_t hash[ETHERS_KECCAK256_LENGTH];
 
-    //Serial.println(Util::ConvertBytesToHex(msgBytes, length).c_str());
+  //Serial.println(Util::ConvertBytesToHex(msgBytes, length).c_str());
 
-    Crypto::Keccak256((uint8_t *)msgBytes, length, hash);
-    Serial.print("Got Hash: ");
-    Serial.println(Util::ConvertBytesToHex(hash, ETHERS_KECCAK256_LENGTH).c_str());
-    crypto->Sign(hash, signature);
-    Serial.print("Got Sig: ");
-    Serial.println(Util::ConvertBytesToHex(signature, ETHERS_SIGNATURE_LENGTH).c_str());
+  Crypto::Keccak256((uint8_t*)msgBytes, length, hash);
+  Serial.print("Got Hash: ");
+  Serial.println(Util::ConvertBytesToHex(hash, ETHERS_KECCAK256_LENGTH).c_str());
+  crypto->Sign(hash, signature);
+  Serial.print("Got Sig: ");
+  Serial.println(Util::ConvertBytesToHex(signature, ETHERS_SIGNATURE_LENGTH).c_str());
 }
 
 void KeyID::initPrivateKey(const std::string& privateKey, Web3* web3)
 {
-    crypto = new Crypto(web3);
-    crypto->SetPrivateKey(privateKey.c_str());
-    recoveredKey = true;
-    Serial.print("Device Address: ");
-    Serial.println(getAddress().c_str());
+  crypto = new Crypto(web3);
+  crypto->SetPrivateKey(privateKey.c_str());
+  recoveredKey = true;
+  Serial.print("Device Address: ");
+  Serial.println(getAddress().c_str());
 }
 
 const std::string KeyID::getAddress()
 {
-    BYTE ethAddressBytes[ETHERS_PUBLICKEY_LENGTH];
-    BYTE publicKey[ETHERS_PUBLICKEY_LENGTH];
+  BYTE ethAddressBytes[ETHERS_PUBLICKEY_LENGTH];
+  BYTE publicKey[ETHERS_PUBLICKEY_LENGTH];
 
-    Crypto::PrivateKeyToPublic(privateKeyBytes, publicKey);
-    Crypto::PublicKeyToAddress(publicKey, ethAddressBytes);
-    //Serial.print("Address: ");
-    std::string address = Util::ConvertBytesToHex(ethAddressBytes, ETHERS_ADDRESS_LENGTH);
-    //Serial.println(address.c_str());
-    return address;
+  Crypto::PrivateKeyToPublic(privateKeyBytes, publicKey);
+  Crypto::PublicKeyToAddress(publicKey, ethAddressBytes);
+  //Serial.print("Address: ");
+  std::string address = Util::ConvertBytesToHex(ethAddressBytes, ETHERS_ADDRESS_LENGTH);
+  //Serial.println(address.c_str());
+  return address;
 }
